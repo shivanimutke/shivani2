3a63a23525735c25bd924673ff529b0c578247d4
shivani mutke <42477972+shivanimutke@users.noreply.github.com>
2020-05-26 20:35:41 +0530

    Add files via upload

---
+++ assets/firsttrywebpage/dom-main.js
@@ -0,0 +1,30 @@
+//tut62
+var element;
+//element=document;console will write all html file
+element=document.getElementById("firstlist").set;
+console.log(element);
+//it always retuns in array
+/*
+dom-main.js:4 ƒ getElementById() { [native code] }
+document.getElementById
+ƒ getElementById() { [native code] }
+document.getElementById;
+ƒ getElementById() { [native code] }
+document.getElementById("header")
+null
+document.getElementsByClassName
+ƒ getElementsByClassName() { [native code] }
+document.getElementsByClassName("bgimage");
+HTMLCollection [div.bgimage]length: 10: div.bgimage__proto__: HTMLCollection
+document.getElementsByClassName("bgimage")[0]
+<div class=​"bgimage">​<div class=​"menu">​…​</div>​<div class=​"text">​…​</div>​</div>​
+document.getElementsByClassName("bgimage")[1];
+undefined
+document.getElementsByTagNameName("bgimage");
+VM1971:1 Uncaught TypeError: document.getElementsByTagNameName is not a function
+    at <anonymous>:1:10
+(anonymous) @ VM1971:1
+document.getElementsByTagName("bgimage");
+HTMLCollection []length: 0__proto__: HTMLCollection
+document.getElementsByTagName
+ƒ getElementsByTagName() { [native code] }*/
